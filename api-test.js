71146b2f606eeb415acb024118062430e452b9ad
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-04-08 20:03:38 -0500

    Add metrics for login errors and response time in k6 load testing script

--- api-test.js
+++ api-test.js
@@ -1,7 +1,11 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { Rate, Trend } from 'k6/metrics';
 import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 import { SharedArray } from 'k6/data';
+
+const loginErrors = new Rate('login_errors');
+const loginResponseTime = new Trend('login_response_time', true);
 
 const users = new SharedArray('users', function () {
   return papaparse.parse(open('./users.csv'), { header: true })
@@ -42,6 +46,9 @@ export default function () {
       }
     },
   });
+
+  loginResponseTime.add(loginRes.timings.duration);
+  loginErrors.add(!loginOk);
 
   if (!loginOk) {
     console.error(`Fallo Login: Status ${loginRes.status} - Body: ${loginRes.body} - User: ${user.user}`);
